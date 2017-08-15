9b4f28cd82801fc128cbb210b06362a37a4d80c7
Christophe Hurpeau <christophe@hurpeau.com>
2017-08-15 19:24:35 +0200

    chore: update dependencies

--- packages/babel-preset-pob-react/lib/index.js
+++ packages/babel-preset-pob-react/lib/index.js
@@ -2,12 +2,16 @@ const useDefault = obj => (obj.__esModule ? obj.default : obj);
 const presetReact = useDefault(require('babel-preset-react'));
 const pluginReactRequire = useDefault(require('babel-plugin-react-require'));
 const pluginTransformReactJSXSelf = useDefault(require('babel-plugin-transform-react-jsx-self'));
-const pluginTransformReactJSXSource = useDefault(require('babel-plugin-transform-react-jsx-source'));
+const pluginTransformReactJSXSource = useDefault(
+  require('babel-plugin-transform-react-jsx-source')
+);
 
-
-module.exports = function (context, opts = {}) {
-  const production = opts.production !== undefined ? opts.production : (process.env.NODE_ENV === 'production');
-  if (typeof production !== 'boolean') throw new Error("Preset pob-react 'production' option must be a boolean.");
+module.exports = function(context, opts = {}) {
+  const production =
+    opts.production !== undefined ? opts.production : process.env.NODE_ENV === 'production';
+  if (typeof production !== 'boolean') {
+    throw new Error("Preset pob-react 'production' option must be a boolean.");
+  }
 
   return {
     presets: presetReact,
