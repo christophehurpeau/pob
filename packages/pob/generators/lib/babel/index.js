39f506c163099ac88665e78457e59660c76823d0
Christophe Hurpeau <christophe@hurpeau.com>
2018-04-22 00:04:24 +0200

    fix(pob-babel): typings path

--- packages/pob/generators/lib/babel/index.js
+++ packages/pob/generators/lib/babel/index.js
@@ -149,7 +149,7 @@ module.exports = class BabelGenerator extends Generator {
     packageUtils.addDevDependencies(pkg, {
       '@babel/core': '^7.0.0-beta.44',
       'babel-core': '7.0.0-bridge.0',
-      'pob-babel': '^22.0.1',
+      'pob-babel': '^22.0.2',
     });
 
     packageUtils.addOrRemoveDevDependencies(pkg, packageUtils.hasReact(pkg), {
