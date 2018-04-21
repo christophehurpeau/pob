5bbe2686e8cfabf8866ddfa93c11762d66b56180
Christophe Hurpeau <christophe@hurpeau.com>
2018-04-21 23:54:52 +0200

    fix: typescript redeclare global fixed

--- packages/pob-babel/types/globals.d.ts
+++
@@ -1,3 +0,0 @@
-declare const POB_ENV: 'production' | 'development';
-declare const POB_TARGET: 'node' | 'browser';
-declare const POB_TARGET_VERSION: undefined | string;
--- packages/pob-babel/types/index.ts
+++ packages/pob-babel/types/index.ts
@@ -1,1 +1,7 @@
-import './globals.d.ts';
+export {}
+
+declare global {
+  const POB_ENV: 'production' | 'development';
+  const POB_TARGET: 'node' | 'browser';
+  const POB_TARGET_VERSION: undefined | string;
+}
