8a820f1c97ee80aaf3810950d5189bc1d4ecc591
Christophe Hurpeau <christophe@hurpeau.com>
2018-04-27 11:52:37 +0200

    feat(pob): update pob-babel and pob-release

--- packages/pob/generators/lib/index.js
+++ packages/pob/generators/lib/index.js
@@ -412,7 +412,7 @@ module.exports = class PobLibGenerator extends Generator {
         delete pkg.scripts.version;
       }
     } else {
-      packageUtils.addDevDependency(pkg, 'pob-release', '^4.0.0');
+      packageUtils.addDevDependency(pkg, 'pob-release', '^4.1.1');
       packageUtils.addScripts(pkg, {
         release: 'pob-repository-check-clean && pob-release',
         preversion: ['yarn run lint', withBabel && 'yarn run build', this.pobjson.documentation && 'yarn run generate:docs', 'pob-repository-check-clean']
