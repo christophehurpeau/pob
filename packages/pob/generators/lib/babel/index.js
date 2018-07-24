77518d7c21c5e1f987083970a0a556f4fe88e035
Christophe Hurpeau <christophe@hurpeau.com>
2018-07-24 23:35:31 +0200

    feat: update babel 7

--- packages/pob/generators/lib/babel/index.js
+++ packages/pob/generators/lib/babel/index.js
@@ -78,13 +78,13 @@ module.exports = class BabelGenerator extends Generator {
     /* dependencies */
 
     packageUtils.addOrRemoveDevDependencies(pkg, useBabel, {
-      '@babel/core': '^7.0.0-beta.52',
+      '@babel/core': '^7.0.0-beta.54',
       'babel-core': '7.0.0-bridge.0',
       'pob-babel': '^22.3.2',
     });
 
     packageUtils.addOrRemoveDevDependencies(pkg, useBabel && packageUtils.hasReact(pkg), {
-      '@babel/preset-react': '^7.0.0-beta.52',
+      '@babel/preset-react': '^7.0.0-beta.54',
     });
 
     packageUtils.removeDevDependencies(pkg, [
@@ -96,7 +96,7 @@ module.exports = class BabelGenerator extends Generator {
     packageUtils.addOrRemoveDevDependencies(
       pkg,
       this.babelEnvs.find(env => env.target === 'browser' && env.version === undefined),
-      { '@babel/preset-env': '^7.0.0-beta.52' },
+      { '@babel/preset-env': '^7.0.0-beta.54' },
     );
 
     packageUtils.addOrRemoveDevDependencies(
