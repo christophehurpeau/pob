465e1fb5f7003654f8d4a180b66fe22b5bdc5aaf
Christophe Hurpeau <christophe@hurpeau.com>
2018-10-09 23:20:37 +0200

    fix: remove unused var

--- packages/pob-babel/lib/createRollupConfig.js
+++ packages/pob-babel/lib/createRollupConfig.js
@@ -32,10 +32,6 @@ const nodeVersion = version => {
   }
 };
 
-const externalModules = nodeBuiltinModules
-  .concat(Object.keys(pkg.dependencies || {}))
-  .concat(Object.keys(pkg.peerDependencies || {}));
-
 // allow to resolve .ts entry files
 /* eslint-disable node/no-deprecated-api */
 if (!require.extensions['.ts']) {
