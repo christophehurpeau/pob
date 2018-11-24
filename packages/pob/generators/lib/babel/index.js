3850905a7285fd30500a4e4f65de462daabb81bf
Christophe Hurpeau <christophe@hurpeau.com>
2018-11-24 16:04:04 +0100

    fix: pkg module:node-dev path

--- packages/pob/generators/lib/babel/index.js
+++ packages/pob/generators/lib/babel/index.js
@@ -216,7 +216,7 @@ module.exports = class BabelGenerator extends Generator {
 
     if (esNodeEnv) {
       pkg['module:node'] = `./dist/index-${esNodeEnv.target}${esNodeEnv.version}.es.js`;
-      pkg['module:node-dev'] = './dist/index-node8-dev.es.js';
+      pkg['module:node-dev'] = `./dist/index-${esNodeEnv.target}${esNodeEnv.version}-dev.es.js`;
     }
 
     if (useBabel) {
