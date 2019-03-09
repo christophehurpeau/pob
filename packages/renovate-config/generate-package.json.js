548a6a4a417931ad3db6c4d150acc1aa168c2179
Christophe Hurpeau <christophe@hurpeau.com>
2019-03-09 19:46:46 +0100

    feat: add @pob/renovate-config

---
+++ packages/renovate-config/generate-package.json.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const fs = require('fs');
+const prettier = require('prettier');
+const pobDependencies = require('../pob-dependencies/package.json');
+
+const path = './package.json';
+const pkg = JSON.parse(fs.readFileSync(path, 'utf-8'));
+
+pkg["renovate-config"].default.packageRules[0].packageNames = Object.keys(pobDependencies.devDependencies);
+
+const formattedPkg = prettier.format(JSON.stringify(pkg, null, 2), {
+  parser: 'json',
+  printWidth: 80,
+});
+
+fs.writeFileSync('./package.json', formattedPkg);
