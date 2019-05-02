c464e4d12fe1c6db269bbc191168308e8c58fb99
Christophe Hurpeau <christophe@hurpeau.com>
2019-05-02 21:13:50 +0200

    chore: remove yarn-update-lock and rollup-config-external-dependencies

--- packages/yarn-update-lock/lib/yarn-deduplicate-lock.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-
-'use strict';
-
-require('yarn-deduplicate/cli.js');
--- packages/yarn-update-lock/lib/yarn-update-lock.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-# Any subsequent(*) commands which fail will cause the shell script to exit immediately
-
-yarn --prefer-offline
-yarn run yarn-deduplicate
-yarn --prefer-offline
