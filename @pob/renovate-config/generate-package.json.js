a29f6e92688c7c98caeb7bdebcf9aaffcecb9fc3
Christophe Hurpeau <christophe@hurpeau.com>
2019-08-30 00:55:27 +0200

    chore: lint renovate-config and add npmignore

--- @pob/renovate-config/generate-package.json.js
+++ @pob/renovate-config/generate-package.json.js
@@ -7,7 +7,9 @@ const pobDependencies = require('pob-dependencies/package.json');
 const path = './package.json';
 const pkg = JSON.parse(fs.readFileSync(path, 'utf-8'));
 
-pkg["renovate-config"].default.packageRules[0].packageNames = Object.keys(pobDependencies.devDependencies);
+pkg['renovate-config'].default.packageRules[0].packageNames = Object.keys(
+  pobDependencies.devDependencies
+);
 
 const formattedPkg = prettier.format(JSON.stringify(pkg, null, 2), {
   parser: 'json',
