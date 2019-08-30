ddb8214516cb3e7011347ecfac2f899f7215ec1e
Christophe Hurpeau <christophe@hurpeau.com>
2019-08-30 16:07:03 +0200

    fix: bring back index.d.ts in pob-babel

---
+++ packages/pob-babel/lib/index.d.ts
@@ -0,0 +1,4 @@
+export const PRODUCTION: boolean;
+export const POB_ENV: 'production' | 'development';
+export const POB_TARGET: 'node' | 'browser';
+export const POB_TARGET_VERSION: undefined | string;
