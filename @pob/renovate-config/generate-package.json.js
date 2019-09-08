4438bb7e9009c8910361971ef4b60eb922b36fbc
Christophe Hurpeau <christophe@hurpeau.com>
2019-09-09 00:14:58 +0200

    feat: automerge @pob/repo-config, husky and prettier on patch and minor

--- @pob/renovate-config/generate-package.json.js
+++ @pob/renovate-config/generate-package.json.js
@@ -7,9 +7,11 @@ const pobDependencies = require('pob-dependencies/package.json');
 const path = './package.json';
 const pkg = JSON.parse(fs.readFileSync(path, 'utf-8'));
 
+const excludePkgNames = ['@pob/repo-config', 'husky'];
+
 pkg['renovate-config'].default.packageRules[0].packageNames = Object.keys(
   pobDependencies.devDependencies
-);
+).filter((pkgName) => !excludePkgNames.includes(pkgName));
 
 const formattedPkg = prettier.format(JSON.stringify(pkg, null, 2), {
   parser: 'json',
