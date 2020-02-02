8ee52a8cf0fdefba01926bebfcb5fef72ac689fa
Christophe Hurpeau <christophe@hurpeau.com>
2020-02-03 00:00:39 +0100

    fix: disable yarnhook for yarn 2

--- @pob/repo-config/createHuskyConfig.js
+++ @pob/repo-config/createHuskyConfig.js
@@ -1,6 +1,19 @@
 'use strict';
 
 const path = require('path');
+const semver = require('semver');
+const whichPmRuns = require('which-pm-runs');
+
+const pm = whichPmRuns();
+
+if (pm.name !== 'yarn') {
+  console.error(
+    `Package manager not supported: ${pm.name}. Please run with yarn !`
+  );
+  process.exit(1);
+}
+
+const yarnMajorVersion = semver.major(pm.version);
 
 // eslint-disable-next-line import/no-dynamic-require
 const pkg = require(path.resolve('package.json'));
@@ -13,10 +26,15 @@ module.exports = () => {
   const hooks = {
     'commit-msg': 'commitlint -e $HUSKY_GIT_PARAMS',
     'pre-commit': 'lint-staged',
-    'post-checkout': 'yarnhook',
-    'post-merge': 'yarnhook',
-    'post-rewrite': 'yarnhook',
   };
+
+  if (yarnMajorVersion < 2) {
+    Object.assign(hooks, {
+      'post-checkout': 'yarnhook',
+      'post-merge': 'yarnhook',
+      'post-rewrite': 'yarnhook',
+    });
+  }
 
   if (shouldRunTest()) hooks['pre-push'] = 'cross-env CI=true yarn test';
 
