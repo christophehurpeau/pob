fdc26ff8300067c1ceae1e9f3600ba42b5a77ac5
Christophe Hurpeau <christophe@hurpeau.com>
2020-02-02 23:10:35 +0100

    feat: support yarn 2

--- @pob/repo-config/createLintStagedConfig.js
+++ @pob/repo-config/createLintStagedConfig.js
@@ -1,6 +1,21 @@
+/* eslint-disable no-console, unicorn/no-process-exit */
+
 'use strict';
 
 const path = require('path');
+const semver = require('semver');
+const whichPmRuns = require('which-pm-runs');
+
+const pm = whichPmRuns();
+
+if (pm.name !== 'yarn') {
+  console.error(
+    `Package manager not supported: ${pm.name}. Please run with yarn !`
+  );
+  process.exit(1);
+}
+
+const yarnMajorVersion = semver.major(pm.version);
 
 // eslint-disable-next-line import/no-dynamic-require
 const pkg = require(path.resolve('package.json'));
@@ -24,11 +39,12 @@ module.exports = function createLintStagedConfig() {
             .map((workspacePath) => `${workspacePath}/package.json`)
             .join(',')}`
         : ''
-    }}`]: (filenames) => [
-      'yarn --prefer-offline',
-      'yarn-deduplicate',
-      'git add yarn.lock',
-    ],
+    }}`]: (filenames) =>
+      [
+        yarnMajorVersion < 2 ? 'yarn --prefer-offline' : 'yarn',
+        yarnMajorVersion < 2 ? 'yarn-deduplicate' : undefined,
+        `git add yarn.lock${yarnMajorVersion}` >= 2 ? ' .yarn .yarnrc.yml' : '',
+      ].filter(Boolean),
     [`{.eslintrc.json,package.json${
       workspaces
         ? `,${workspaces
---
+++ @pob/repo-config/lint-staged.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+'use strict';
+
+require('lint-staged/bin/lint-staged.js');
