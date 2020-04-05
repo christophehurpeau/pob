303b3ee6950faa3fce42671e3a28484bf23d620b
Christophe Hurpeau <christophe@hurpeau.com>
2020-04-05 17:03:57 +0200

    refactor: rewrite pob-babel bin in js

---
+++ packages/pob-babel/bin/pob-watch.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const { spawnSync } = require('child_process');
+const argv = require('minimist-argv');
+
+if (argv.clean !== false) {
+  fs.rmdirSync(path.resolve('dist'), { recursive: true });
+}
+
+const rollupBin = require.resolve('rollup/dist/bin/rollup');
+const configPath = require.resolve('../rollup.config.js');
+
+const { error } = spawnSync(rollupBin, ['--watch', '--config', configPath], {
+  stdio: 'inherit',
+});
+if (error) {
+  throw error;
+}
