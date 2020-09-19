73ed80765928b06e590a844dfcceffd1f4b521d7
Christophe Hurpeau <christophe@hurpeau.com>
2020-09-19 23:40:12 +0200

    fix: ignore typedoc-plugin-lerna-packages in update-peerdeps script

--- packages/pob-dependencies/scripts/update-peerdeps.js
+++ packages/pob-dependencies/scripts/update-peerdeps.js
@@ -10,6 +10,7 @@ const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf-8'));
 let madeModifications = false;
 
 Object.keys(pkg.devDependencies).forEach((key) => {
+  if (key === 'typedoc-plugin-lerna-packages') return;
   const depPkg = require(`${key}/package.json`);
   if (!depPkg.peerDependencies) return;
   Object.keys(depPkg.peerDependencies).forEach((peerDep) => {
