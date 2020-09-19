60ececed3028d118bb899ea723cf735a0eb27b95
Christophe Hurpeau <christophe@hurpeau.com>
2020-09-19 17:42:00 +0200

    fix: eslint resolve-plugins-relative-to with lerna for eslint 7

--- packages/pob/lib/generators/monorepo/lerna/index.js
+++ packages/pob/lib/generators/monorepo/lerna/index.js
@@ -155,7 +155,7 @@ module.exports = class LernaGenerator extends Generator {
     packageUtils.addScripts(pkg, {
       lint:
         monorepoConfig && monorepoConfig.eslint
-          ? 'eslint --ext js,ts,tsx .'
+          ? 'eslint --resolve-plugins-relative-to . --ext js,ts,tsx .'
           : 'lerna run --stream lint',
       preversion: [
         monorepoConfig && monorepoConfig.eslint
