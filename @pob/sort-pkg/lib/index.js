88bf50b3f150ed60fbcaf0b1e12f3821bf047a96
Christophe Hurpeau <christophe@hurpeau.com>
2020-11-13 22:34:53 +0100

    feat: add dependenciesMeta

--- @pob/sort-pkg/lib/index.js
+++ @pob/sort-pkg/lib/index.js
@@ -69,6 +69,7 @@ module.exports = function sortPkg(pkg) {
     'devPeerDependencies',
     'dependencies',
     'devDependencies',
+    'dependenciesMeta',
     'bundledDependencies',
     'bundleDependencies',
     'optionalDependencies',
