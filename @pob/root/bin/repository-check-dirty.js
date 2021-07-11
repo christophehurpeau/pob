7f755fb997f245e06817e4cd692b6e897468be86
Christophe Hurpeau <christophe@hurpeau.com>
2021-07-11 23:52:40 +0200

    fix(@pob/root): pkg.bin can be a string

--- @pob/root/bin/repository-check-dirty.js
+++ @pob/root/bin/repository-check-dirty.js
@@ -6,4 +6,7 @@ const path = require('path');
 const pkg = require('repository-check-dirty/package.json');
 
 // eslint-disable-next-line import/no-dynamic-require
-require(path.join('repository-check-dirty', pkg.bin['repository-check-dirty']));
+require(path.join(
+  'repository-check-dirty',
+  typeof pkg.bin === 'string' ? pkg.bin : pkg.bin['repository-check-dirty'],
+));
--- @pob/root/bin/yarnhook.js
+++ @pob/root/bin/yarnhook.js
@@ -6,4 +6,7 @@ const path = require('path');
 const pkg = require('yarnhook/package.json');
 
 // eslint-disable-next-line import/no-dynamic-require
-require(path.join('yarnhook', pkg.bin));
+require(path.join(
+  'yarnhook',
+  typeof pkg.bin === 'string' ? pkg.bin : pkg.bin.yarnhook,
+));
