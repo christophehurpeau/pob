7a641738cb40c34170bde3d9911400e5298d64c8
Christophe Hurpeau <christophe@hurpeau.com>
2021-07-11 23:35:57 +0200

    feat: create typedoc renovate update workflow

--- @pob/root/bin/postinstall/install-github-workflows.js
+++ @pob/root/bin/postinstall/install-github-workflows.js
@@ -4,19 +4,35 @@ const fs = require('fs');
 const path = require('path');
 const semver = require('semver');
 
+const ensureWorkflowUninstalled = (workflowName) => {
+  try {
+    fs.unlinkSync(path.resolve(`.github/workflows/${workflowName}.yml`));
+  } catch {}
+};
+
+const installWorkflow = (workflowName, condition = true) => {
+  if (condition) {
+    fs.writeFileSync(
+      path.resolve(`.github/workflows/${workflowName}.yml`),
+      fs.readFileSync(
+        path.resolve(__dirname, `../../github-workflows/${workflowName}.yml`),
+      ),
+    );
+  } else {
+    ensureWorkflowUninstalled(workflowName);
+  }
+};
+
 module.exports = function installGithubWorkflows({ pkg, pm }) {
   const yarnMajorVersion = pm.name === 'yarn' && semver.major(pm.version);
   const isYarnBerry = pm.name === 'yarn' && yarnMajorVersion >= 2;
 
   if (!isYarnBerry) return;
 
-  fs.writeFileSync(
-    path.resolve('.github/workflows/push-renovate-prettier.yml'),
-    fs.readFileSync(
-      path.resolve(
-        __dirname,
-        '../../github-workflows/push-renovate-prettier.yml',
-      ),
-    ),
+  installWorkflow('push-renovate-prettier');
+
+  installWorkflow(
+    'push-renovate-typedoc',
+    !!(pkg.devDependencies && pkg.devDependencies.typedoc),
   );
 };
