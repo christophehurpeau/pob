444d9a7a881306e07f38aec41d61cb4807b4f409
Christophe Hurpeau <christophe@hurpeau.com>
2021-08-10 21:52:20 +0200

    chore(pob-dependencies): update script update-peerdeps

--- packages/pob-dependencies/scripts/update-peerdeps.js
+++ packages/pob-dependencies/scripts/update-peerdeps.js
@@ -12,21 +12,22 @@ Object.keys(pkg.devDependencies).forEach((key) => {
   if (!depPkg.peerDependencies) return;
   Object.keys(depPkg.peerDependencies).forEach((peerDep) => {
     const peerDepRange = depPkg.peerDependencies[peerDep];
-    if (
-      !pkg.devDependencies[peerDep] ||
-      !semver.satisfies(pkg.devDependencies[peerDep], peerDepRange)
-    ) {
+    if (!pkg.devDependencies[peerDep]) {
+      console.warn(
+        `Missing peerdep "${peerDep}" from "${depPkg.name}", asking for "${peerDepRange}"`,
+      );
+    } else if (!semver.satisfies(pkg.devDependencies[peerDep], peerDepRange)) {
       const newVersion = semver.minVersion(peerDepRange).version;
       if (semver.lt(newVersion, pkg.devDependencies[peerDep])) {
         console.warn(
-          `Incompatible peerdep ${peerDep} required version ${newVersion} from ${depPkg.name}, asking for ${peerDepRange}`,
+          `Incompatible peerdep "${peerDep}" required version "${newVersion}" from "${depPkg.name}", asking for ${peerDepRange}`,
         );
       } else {
         console.log(
-          `update ${peerDep} to ${newVersion} because ${
+          `update "${peerDep}" to "${newVersion}" because ${
             !pkg.devDependencies[peerDep]
               ? 'it was added in'
-              : `version ${pkg.devDependencies[peerDep]} doesn't match peer dependency in`
+              : `version "${pkg.devDependencies[peerDep]}" doesn't match peer dependency in`
           } ${depPkg.name} (${depPkg.version})`,
         );
         pkg.devDependencies[peerDep] = newVersion;
