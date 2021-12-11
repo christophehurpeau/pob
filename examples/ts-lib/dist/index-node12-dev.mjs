3d8168f1d93748d0007791ecc54c795cca3de18b
Christophe Hurpeau <christophe@hurpeau.com>
2021-12-11 07:22:51 +0100

    test: update examples

--- examples/ts-lib/dist/index-node12-dev.mjs
+++
@@ -1,6 +0,0 @@
-function simpleLib() {
-  return 'hello world';
-}
-
-export { simpleLib as default };
-//# sourceMappingURL=index-node12-dev.mjs.map
---
+++ examples/ts-lib/dist/index-node14.mjs
@@ -0,0 +1,6 @@
+function simpleLib() {
+  return 'hello world';
+}
+
+export { simpleLib as default };
+//# sourceMappingURL=index-node14.mjs.map
