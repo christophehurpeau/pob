cb78fe8ddd709529a8f026d568e9ad41700d1270
Christophe Hurpeau <christophe@hurpeau.com>
2021-12-12 21:50:35 +0100

    fix: exitCode when lint-staged fails

--- @pob/root/bin/pob-root-lint-staged.mjs
+++ @pob/root/bin/pob-root-lint-staged.mjs
@@ -6,4 +6,11 @@ import lintStaged from 'lint-staged';
 lintStaged({
   concurrent: true,
   relative: true,
-});
+})
+  .then((passed) => {
+    process.exitCode = passed ? 0 : 1;
+  })
+  // eslint-disable-next-line unicorn/prefer-top-level-await
+  .catch(() => {
+    process.exitCode = 1;
+  });
