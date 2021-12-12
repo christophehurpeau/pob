0163505eecd7ca39c07dfe6a969184806e3d2630
Christophe Hurpeau <christophe@hurpeau.com>
2021-12-12 18:18:56 +0100

    fix(pob): add @ornikar/eslint-configs in dependencies

--- packages/pob/lib/utils/dependenciesPackages.cjs
+++ packages/pob/lib/utils/dependenciesPackages.cjs
@@ -1,10 +1,7 @@
 'use strict';
 
-// eslint-disable-next-line import/no-extraneous-dependencies
 exports.pobEslintConfig = require('@pob/eslint-config/package.json');
 
-// eslint-disable-next-line import/no-extraneous-dependencies
 exports.pobEslintConfigTypescript = require('@pob/eslint-config-typescript/package.json');
 
-// eslint-disable-next-line import/no-extraneous-dependencies
 exports.pobEslintConfigTypescriptReact = require('@pob/eslint-config-typescript-react/package.json');
