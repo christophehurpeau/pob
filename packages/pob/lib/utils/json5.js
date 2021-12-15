2a213b545be5e653b4f0a279983ea4cfeaacaa11
Christophe Hurpeau <christophe@hurpeau.com>
2021-12-15 23:41:57 +0100

    fix: defaults for readJSON5 method

--- packages/pob/lib/utils/json5.js
+++ packages/pob/lib/utils/json5.js
@@ -1,7 +1,7 @@
 import JSON5 from 'json5';
 
 export function readJSON5(fs, destinationPath, defaults) {
-  const content = fs.read(destinationPath, null);
+  const content = fs.read(destinationPath, { defaults: null });
   if (content === null) return defaults;
   return JSON5.parse(content);
 }
