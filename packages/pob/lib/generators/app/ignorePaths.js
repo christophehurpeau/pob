cd1a4e40139a75fb3f183bb9dd93fe1877455ec0
Christophe Hurpeau <christophe@hurpeau.com>
2022-04-24 18:04:39 +0200

    feat: add .env.local for nextjs paths

--- packages/pob/lib/generators/app/ignorePaths.js
+++ packages/pob/lib/generators/app/ignorePaths.js
@@ -1,6 +1,12 @@
 export const appIgnorePaths = {
   alp: (config) => ['# alp paths', '/build', '/public', '/data'],
-  'next.js': (config) => ['# next.js paths', '/.next', '/out', '/build'],
+  'next.js': (config) => [
+    '# next.js paths',
+    '/.next',
+    '/out',
+    '/build',
+    '/.env.local',
+  ],
   remix: (config) => ['# remix paths', '/.cache', '/build', '/public/build'],
   pobpack: (config) => ['/build', '/public'],
   node: (config) => ['/build'],
