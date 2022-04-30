184d92adb92a8982f2a5d173d9e1c32c7acfa2ad
Christophe Hurpeau <christophe@hurpeau.com>
2022-04-30 13:31:32 +0200

    fix(pob-babel): export index for tests

---
+++ packages/pob-babel/lib/index.js
@@ -0,0 +1,6 @@
+/* Only used in tests */
+export const IS_DEV = false;
+export const IS_PROD = false;
+export const IS_TEST = true;
+export const POB_TARGET = 'node';
+export const POB_TARGET_VERSION = 'current';
