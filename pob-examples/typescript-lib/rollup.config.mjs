8d24916fefe23c828a57795d6e7ff111e7a32d3b
Christophe Hurpeau <christophe@hurpeau.com>
2022-09-04 14:33:54 +0200

    style: fix prettier

--- pob-examples/typescript-lib/rollup.config.mjs
+++ pob-examples/typescript-lib/rollup.config.mjs
@@ -3,5 +3,5 @@ import createRollupConfig from 'pob-babel/createRollupConfig.js';
 export default createRollupConfig({
   cwd: new URL('.', import.meta.url).pathname.slice(
     process.platform === 'win32' ? 1 : 0,
-  )
+  ),
 });
