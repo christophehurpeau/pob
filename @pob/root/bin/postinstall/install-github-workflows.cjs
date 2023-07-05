a4cc71cca028edad8a0dc9fcb37c2187c1804273
Christophe Hurpeau <christophe@hurpeau.com>
2023-07-06 00:47:59 +0200

    fix(@pob/root): only add workflow build if a build script exists

--- @pob/root/bin/postinstall/install-github-workflows.cjs
+++ @pob/root/bin/postinstall/install-github-workflows.cjs
@@ -33,7 +33,11 @@ module.exports = function installGithubWorkflows({ pkg, pm }) {
     installWorkflow('push-renovate-pob_root');
     installWorkflow('push-renovate-prettier');
     ensureWorkflowUninstalled('push-renovate-typedoc');
-    if (pkg.devDependencies && pkg.devDependencies.rollup) {
+    if (
+      pkg.devDependencies &&
+      pkg.devDependencies.rollup &&
+      pkg.scripts?.build
+    ) {
       installWorkflow('push-renovate-build');
     } else {
       ensureWorkflowUninstalled('push-renovate-build');
