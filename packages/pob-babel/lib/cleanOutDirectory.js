42b4885b23ed6d9534bb2ebff36c0ed406408f3b
Christophe Hurpeau <christophe@hurpeau.com>
2023-10-18 22:25:57 +0200

    fix: clean out directory should clean tsc cache too

--- packages/pob-babel/lib/cleanOutDirectory.js
+++ packages/pob-babel/lib/cleanOutDirectory.js
@@ -3,8 +3,10 @@ import path from 'node:path';
 
 export function cleanOutDirectory({ outDirectory = 'dist' } = {}) {
   if (fs.rmSync) {
+    fs.rmSync('node_modules/.cache/tsc', { recursive: true, force: true });
     fs.rmSync(path.resolve(outDirectory), { recursive: true, force: true });
   } else {
+    fs.rmdirSync('node_modules/.cache/tsc', { recursive: true, force: true });
     fs.rmdirSync(path.resolve(outDirectory), { recursive: true, force: true });
   }
 }
