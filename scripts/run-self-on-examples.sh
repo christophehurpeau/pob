e8d88c7be2b72808e240df2c7ace925d8a4a85c7
Christophe Hurpeau <302891+christophehurpeau@users.noreply.github.com>
2024-03-06 21:52:51 +0100

    chore: script run-self-on-examples

---
+++ scripts/run-self-on-examples.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# exit on error
+set -e
+
+# get list of changed files in git
+changed_files=$(git diff --name-only HEAD)
+
+for example in pob-examples/*; do
+  echo "Running pob on $example"
+  cd $example
+  ../../packages/pob/lib/pob.js update --force || exit 1
+  cd ../..
+done
+
+# get new changed files, if there are new, exit with error
+new_changed_files=$(git diff --name-only HEAD)
+if [ "$changed_files" != "$new_changed_files" ]; then
+  echo "There are new changes in the examples, please check and commit them if needed"
+  exit 1
+fi
+
