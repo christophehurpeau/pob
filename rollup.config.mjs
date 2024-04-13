014e815abd94bbb5ed9576494026c4a5259d8e6d
Christophe Hurpeau <302891+christophehurpeau@users.noreply.github.com>
2024-04-13 23:06:59 +0200

    chore: add missing rollup config for rollup-esbuild

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,5 +1,6 @@
-import configs1 from './pob-examples/typescript-lib-with-rollup-typescript/rollup.config.mjs';
-import configs2 from './pob-examples/typescript-lib/rollup.config.mjs';
-import configs3 from './packages/yarn-workspace-utils/rollup.config.mjs';
+import configs1 from './pob-examples/typescript-lib-rollup-esbuild/rollup.config.mjs';
+import configs2 from './pob-examples/typescript-lib-with-rollup-typescript/rollup.config.mjs';
+import configs3 from './pob-examples/typescript-lib/rollup.config.mjs';
+import configs4 from './packages/yarn-workspace-utils/rollup.config.mjs';
 
-export default [...configs1, ...configs2, ...configs3];
+export default [...configs1, ...configs2, ...configs3, ...configs4];
