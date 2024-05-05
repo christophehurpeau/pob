8e472a98b0a722c5cd779e4731ff625b0bfa07bb
Christophe Hurpeau <302891+christophehurpeau@users.noreply.github.com>
2024-05-05 13:48:49 +0200

    feat!: change prettier config to get closer to default

--- @pob/root/prettier-config.cjs
+++ @pob/root/prettier-config.cjs
@@ -1,23 +1,12 @@
 'use strict';
 
 module.exports = {
-  trailingComma: 'all',
-  singleQuote: true,
-  // https://github.com/airbnb/javascript/pull/1863
-  arrowParens: 'always',
-
   overrides: [
     // use json-stringify for lerna.json as lerna will reformat it on new version
     {
       files: ['lerna.json'],
       options: {
         parser: 'json-stringify',
-      },
-    },
-    {
-      files: ['*.yml'],
-      options: {
-        singleQuote: false,
       },
     },
     {
