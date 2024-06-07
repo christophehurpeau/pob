eda9a6776e8ac6125ac3349ad1d88c53c923ac8c
Christophe Hurpeau <302891+christophehurpeau@users.noreply.github.com>
2024-06-07 21:34:50 +0200

    chore: update config for yarn-version

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,13 @@
 import configs1 from "./pob-examples/typescript-lib-rollup-esbuild/rollup.config.mjs";
 import configs2 from "./pob-examples/typescript-lib-with-rollup-typescript/rollup.config.mjs";
 import configs3 from "./pob-examples/typescript-lib/rollup.config.mjs";
-import configs4 from "./packages/yarn-workspace-utils/rollup.config.mjs";
+import configs4 from "./packages/yarn-version/rollup.config.mjs";
+import configs5 from "./packages/yarn-workspace-utils/rollup.config.mjs";
 
-export default [...configs1, ...configs2, ...configs3, ...configs4];
+export default [
+  ...configs1,
+  ...configs2,
+  ...configs3,
+  ...configs4,
+  ...configs5,
+];
