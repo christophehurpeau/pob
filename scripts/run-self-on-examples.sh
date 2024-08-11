c60fa8c660dbc6a3507ce958e559f71e9dcee8d5
Christophe Hurpeau <302891+christophehurpeau@users.noreply.github.com>
2024-08-11 18:11:55 +0200

    chore: run yarn build in checks script

--- scripts/run-self-on-examples.sh
+++ scripts/run-self-on-examples.sh
@@ -6,6 +6,8 @@ set -e
 # get list of changed files in git
 changed_files=$(git diff --name-only HEAD)
 
+yarn build
+
 for example in pob-examples/*; do
   echo "Running pob on $example"
   cd $example
