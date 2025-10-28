540c840bdd80eee4b069033c5f713104eb973901
Christophe Hurpeau <302891+christophehurpeau@users.noreply.github.com>
2025-10-28 22:13:54 +0100

    feat: update packageManager

--- packages/pob/lib/generators/core/yarn/CoreYarnGenerator.js
+++ packages/pob/lib/generators/core/yarn/CoreYarnGenerator.js
@@ -142,9 +142,9 @@ export default class CoreYarnGenerator extends Generator {
       if (
         !pkg.packageManager ||
         !pkg.packageManager.startsWith("yarn@") ||
-        lt(pkg.packageManager.slice("yarn@".length), "4.9.3")
+        lt(pkg.packageManager.slice("yarn@".length), "4.10.3")
       ) {
-        pkg.packageManager = "yarn@4.9.3";
+        pkg.packageManager = "yarn@4.10.3";
       }
 
       // must be done after plugins installed
