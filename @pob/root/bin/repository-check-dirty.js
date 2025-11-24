5077642031254d7845ea100e3e65e83fb244d0c9
Christophe Hurpeau <302891+christophehurpeau@users.noreply.github.com>
2025-11-24 23:05:10 +0100

    feat: implement repository check for uncommitted changes in @pob//root directly

--- @pob/root/bin/repository-check-dirty.js
+++ @pob/root/bin/repository-check-dirty.js
@@ -1,3 +1,17 @@
 #!/usr/bin/env node
 
-import "repository-check-dirty/bin/repository-check-dirty.js";
+import { execSync } from "node:child_process";
+
+try {
+  const stdout = execSync("git status --porcelain", { encoding: "utf8" });
+  if (stdout) {
+    console.log(
+      "Repository has uncommitted changes, please commit or remove these files:\n",
+    );
+    console.log(stdout);
+    process.exit(1);
+  }
+} catch (error) {
+  console.error(error.message);
+  process.exit(1);
+}
