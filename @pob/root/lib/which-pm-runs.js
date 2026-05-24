3e86108d9f089e3d522cb8e4b6df0759d764991b
Christophe Hurpeau <302891+christophehurpeau@users.noreply.github.com>
2026-05-24 14:02:27 +0200

    chore: update pnpm detection

--- @pob/root/lib/which-pm-runs.js
+++ @pob/root/lib/which-pm-runs.js
@@ -1,11 +1,13 @@
 export function whichPmRuns() {
   const userAgent = process.env.npm_config_user_agent;
   if (!userAgent) {
-    if (process.env.npm_execpath?.endsWith("/bin/pnpm.mjs")) {
-      const version = process.env.npm_execpath.split("/").slice(-3)[0];
-      console.log("Detected pnpm via npm_execpath with version", version);
-      return { name: "pnpm", version };
+    if (
+      process.env.npm_execpath?.endsWith("/bin/pnpm") ||
+      process.env.npm_execpath?.endsWith("/bin/pnpm.mjs")
+    ) {
+      return { name: "pnpm", version: "unknown" };
     }
+
     return undefined;
   }
 
