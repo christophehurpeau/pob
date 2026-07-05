dab3975be9765f78d302da18434abbe5e130c8ec
Christophe Hurpeau <302891+christophehurpeau@users.noreply.github.com>
2026-07-05 16:50:46 +0200

    fix: throw error for deprecated pob-babel dependency

--- packages/pob/lib/generators/common/babel/CommonBabelGenerator.js
+++ packages/pob/lib/generators/common/babel/CommonBabelGenerator.js
@@ -322,8 +322,7 @@ export default class CommonBabelGenerator extends Generator {
     }
 
     if (pkg.dependencies && pkg.dependencies["pob-babel"]) {
-      // update pob-babel in alp-dev
-      packageUtils.addDependencies(pkg, ["pob-babel"], "^");
+      throw new Error("pob-babel is deprecated.");
     }
     if (pkg.dependencies && pkg.dependencies["@babel/runtime"]) {
       // update pob-babel in alp-dev
